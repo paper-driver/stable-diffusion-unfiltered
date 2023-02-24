bf31e181db798ff77551b49bc8b23ddc8c253ff9
paper-driver <64306121+paper-driver@users.noreply.github.com>
2023-02-24 12:35:15 -0500

    Update txt2img.py

--- scripts/txt2img.py
+++ scripts/txt2img.py
@@ -18,7 +18,7 @@ from ldm.util import instantiate_from_config
 from ldm.models.diffusion.ddim import DDIMSampler
 from ldm.models.diffusion.plms import PLMSSampler
 
-from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
+# from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
 from transformers import AutoFeatureExtractor
 
 
